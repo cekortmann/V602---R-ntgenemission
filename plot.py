ed76da2746ee51923b7628eca5778938312931f4
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2023-06-20 14:26:13 +0200

    lalla

--- plot.py
+++ plot.py
@@ -11,40 +11,31 @@ from uncertainties.unumpy import uarray                     # Array von Fehler: 
 from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
                                   std_devs as stds)         # Abweichung:       stds(fehlerarray) = errarray
 
-# Plot 1:
 
-md = pd.read_csv('tables/md.csv')
-
-nu, U = np.genfromtxt('tables/U.txt', unpack=True, skip_header=1)
-U = U/(2*0.9892978920131985)            # Normierung 
-
-# für den initial guess bei curvefit()
-n = len(nu)                             # Anzahl der Daten
-mean = sum(nu*U)/n                      # Mittelwert
-sigma = np.sqrt(sum(U*(nu - mean)**2))  # Standardabweichung
+z, E = np.genfromtxt('E.txt', unpack=True, skip_header=1)
+#U = U/(2*0.9892978920131985)            # Normierung 
+  # Standardabweichung
 
 # Ausgleichsrechung nach Gaußverteilung
-def g(x,a,x0,b):
-    return a*np.exp(-(x-x0)**2/(b))     # b = 2*sigma**2
+def g(x, a, b):
+    return a*x+b     # b = 2*sigma**2
 
-para, pcov = curve_fit(g, nu, U, p0=[1,mean,sigma])
-a, nu0, b = para
+para, pcov = curve_fit(g, z, np.sqrt(E))
+a, b = para
 pcov = np.sqrt(np.diag(pcov))
-fa, fnu0, fb = pcov
+fa, fb = pcov
 ua = ufloat(a, fa) 
 ub = ufloat(b, fb)
-unu0 = ufloat(nu0, fnu0)
 
-xx = np.linspace(18, 42, 10**4)
+xx = np.linspace(35, 42, 10**4)
 
-plt.plot(nu, U, 'xr', markersize=6 , label = 'Messdaten', alpha=0.5)
+plt.plot(z, np.sqrt(E), 'xr', markersize=6 , label = 'Messdaten', alpha=0.5)
 plt.plot(xx, g(xx, *para), '-b', linewidth = 1, label = 'Ausgleichsfunktion', alpha=0.5)
-plt.xlabel(r'$\nu \, / \, \mathrm{kHz}$')
-plt.ylabel(r'$U_A \, / \, U_E$')
+plt.xlabel(r'$z $')
+plt.ylabel(r'$E \, / \, eV$')
 plt.legend(loc="best")                  # legend position
 plt.grid(True)                          # grid style
-plt.xlim(22, 40)
-plt.ylim(-0.05, 1.05)
+print(ua)
 
 plt.savefig('build/plot.pdf', bbox_inches = "tight")
 plt.clf() 
